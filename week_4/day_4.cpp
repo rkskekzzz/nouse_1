5dadb4f575559ee313476f77c2032a11fad9f302
sutech <wkdlfflxh@naver.com>
2020-12-08 13:21:05 +0900

    week_4

--- week_4/day_4.cpp
+++ week_4/day_4.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
+#include <vector>
 
-void input(){
+int N, M, max;
+std::vector<int> array;
 
+void input()
+{
+	std::cin >> N >> M;
+	array.resize(M);
+	for (int i = 0; i < M; i++)
+	{
+		std::cin >> array[i];
+		max = (array[i] > max) ? array[i] : max;
+	}
 }
 
-void solution(){
+void solution()
+{
+	int start = 1;
+	int mid;
+	int end = max;
+	int div_sum;
 
+	while (start <= end)
+	{
+		mid = (start + end) / 2;
+		div_sum = 0;
+		for (int i = 0; i < M; i++)
+		{
+			div_sum += array[i] / mid;
+			if (array[i] % mid)
+				++div_sum;
+		}
+		if (div_sum > N)
+			start = mid + 1;
+		else
+			end = mid - 1;
+	}
+	std::cout << start;
 }
 
-int main(){
+int main()
+{
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
---
+++ week_4/day_5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int N;
+
+void input(){
+	std::cin >> N;
+}
+
+void solution(){
+
+}
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	input();
+	solution();
+	return 0;
+}
