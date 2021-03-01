438da699c1db9fff66fbf1c31bf49c8247e1a703
su <su@suui-MacBookPro.local>
2021-03-01 16:46:44 +0900

    pf post

--- week_11/pp.cpp
+++ week_11/pp.cpp
@@ -3,7 +3,12 @@
 
 int main()
 {
-	int a = 7;
-	std::cout << std::bitset<2>(a) << std::endl;
+	int arr[8];
+
+	arr[1 << 0] = 1;
+	arr[1 << 1] = 2;
+	std::cout << "arr[1] : " << arr[1] << std::endl;
+	std::cout << "arr[2] : " << arr[2] << std::endl;
+	//std::cout << std::bitset<2>(a) << std::endl;
 }
 
